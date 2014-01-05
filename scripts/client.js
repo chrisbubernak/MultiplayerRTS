536eb901a9d8cc4a4f5ecdf81250fed2d407b701
Chris Bubernak <chris.bubernak@gmail.com>
2014-01-05 11:54:48 -0800

    trying to remove node_modules from source control

--- scripts/client.js
+++ scripts/client.js
@@ -1,19 +1,17 @@
 var myGame;
 
-window.onload = function() {
-var id;
+window.onload = function () {
+    var id;
 
-  var socket = io.connect('/');
-  socket.on('ClientJoined', function (data) {
-    console.log("My user ID is: " + data.userId);
-    id = data.userId;
-  });
-   
+    var socket = io.connect('/');
+    socket.on('ClientJoined', function (data) {
+        console.log("My user ID is: " + data.userId);
+        id = data.userId;
+    });
 
-
-  socket.on('startGame', function(data) {
-  	  myGame = new Game(socket, id, data.clients, data.gameId);
-  	  console.log(data);
-      myGame.run();
-  });
-}
+    socket.on('startGame', function (data) {
+        myGame = new Game(socket, id, data.clients, data.gameId);
+        console.log(data);
+        myGame.run();
+    });
+};
--- scripts/knight.js
+++ scripts/knight.js
@@ -1,41 +1,41 @@
 //class(ish) definition...
-
 function Knight(id, loc, player) {
-  this.id = id;
-  this.loc = loc;
-  this.prevLoc = loc;
-  var coords = utilities.boxToCoords(loc);
-  this.x = coords.x;
-  this.y = coords.y;
-  this.w = 30;
-  this.h = 30;
-  this.player = player;
-  //use this to determine what part of the sprite colelction to use
-  this.imageX = 32*2;
-  this.imageY = 0;
-  this.imageW = 32;
-  this.imageH = 32;
+    this.id = id;
+    this.loc = loc;
+    this.prevLoc = loc;
+    var coords = utilities.boxToCoords(loc);
+    this.x = coords.x;
+    this.y = coords.y;
+    this.w = 30;
+    this.h = 30;
+    this.player = player;
 
-  this.target = new Array();
-  this.attackMax = 10;
-  this.attackMin = 5;
-  this.selected = false;
-  this.color = "black";
-  this.sight = Knight.SIGHT_RANGE;
-  this.totalHealth = 100;
-  this.health = this.totalHealth;
+    //use this to determine what part of the sprite colelction to use
+    this.imageX = 32 * 2;
+    this.imageY = 0;
+    this.imageW = 32;
+    this.imageH = 32;
 
-  this.attackSpeed = 25;
-  this.attackTimer = 0;
+    this.target = new Array();
+    this.attackMax = 10;
+    this.attackMin = 5;
+    this.selected = false;
+    this.color = "black";
+    this.sight = Knight.SIGHT_RANGE;
+    this.totalHealth = 100;
+    this.health = this.totalHealth;
+
+    this.attackSpeed = 25;
+    this.attackTimer = 0;
 }
 
-Knight.prototype.imageReady = function(){
-	return Knight.imageLoaded;
-}
+Knight.prototype.imageReady = function () {
+    return Knight.imageLoaded;
+};
 
-Knight.prototype.getImage = function(){
-	return Knight.image;
-}
+Knight.prototype.getImage = function () {
+    return Knight.image;
+};
 
 Knight.SIGHT_RANGE = 50;
 Knight.WIDTH = 32;
@@ -45,13 +45,7 @@ Knight.HEIGHT = 32;
 Knight.image = new Image();
 Knight.imageLoaded = false;
 var imageLoaded = false;
-Knight.image.onload = function() {
-  Knight.imageLoaded = true;
+Knight.image.onload = function () {
+    Knight.imageLoaded = true;
 };
 Knight.image.src = '/images/knight.png';
-
-
-
-
-
-
