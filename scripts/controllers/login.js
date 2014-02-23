fe7ef9292a1b4832b524fc356d8c85bdf6cdd56e
Chris Bubernak <chris.bubernak@gmail.com>
2014-02-23 13:51:46 -0800

    Login works, but no mechanism for creating accounts yet...also addded a bunch of files & need to reorganize project

---
+++ scripts/controllers/login.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function(){
+  $('#login-form').ajaxForm({
+    success: function(response, status, xhr) {
+      if (status == 'success') {
+        window.location.href = '/home';
+      }
+    },
+    error: function(e) {
+      console.log('Login Failed');
+    }
+  });
+  $('#user-tf').focus();
+});
