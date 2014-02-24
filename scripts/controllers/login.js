ad0f6a3f9c087eb7ffb5f3027195f61e627819e0
Chris Bubernak <chris.bubernak@gmail.com>
2014-02-23 19:45:38 -0800

    Sign & Signup work (no hashing yet tho)

--- scripts/controllers/login.js
+++ scripts/controllers/login.js
@@ -2,11 +2,12 @@
   $('#login-form').ajaxForm({
     success: function(response, status, xhr) {
       if (status == 'success') {
+        document.getElementById('signin-status').innerHTML = '<div style="color: #5CB85C;">Success!</div>';
         window.location.href = '/home';
       }
     },
     error: function(e) {
-      console.log('Login Failed');
+      document.getElementById('signin-status').innerHTML = '<div style="color: #D9534F;">Login Failed</div>';
     }
   });
   $('#user-tf').focus();
---
+++ scripts/controllers/signup.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function(){
+  //redirect to homepage when cancel button is clicked //
+	$('#account-form-btn1').click(function(){ window.location.href = '/';});
+
+	$('#account-form').ajaxForm({
+		success	: function(responseText, status, xhr){
+			if (status == 'success') {
+        console.log('signup was successful');
+      }
+		},
+		error : function(xhr, status, error){
+			console.log('ERROR: ' + xhr.responseText);
+		}
+	});
+	$('#name-tf').focus();
+})
