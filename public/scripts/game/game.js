47951bba89ef8b71f3e0d5e8315546d3dafd403e
Chris Bubernak <chris.bubernak@gmail.com>
2014-06-29 17:04:44 -0700

    css isn't showing up for some reason...trying to fix

--- public/scripts/game/game.js
+++ public/scripts/game/game.js
@@ -224,4 +224,3 @@ var Game = (function () {
     Game.units = new Array();
     return Game;
 })();
-//# sourceMappingURL=game.js.map
--- public/scripts/game/unit.js
+++ public/scripts/game/unit.js
@@ -110,4 +110,3 @@ var Unit = (function (_super) {
     Unit.attackAnimationIncrememt = .2;
     return Unit;
 })(BaseGameEntity);
-//# sourceMappingURL=unit.js.map
