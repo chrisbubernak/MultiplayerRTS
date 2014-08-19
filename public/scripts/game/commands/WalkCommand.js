4c83be7fad9334ac7ea16b0b899cd5182737666a
Chris Bubernak <chris.bubernak@gmail.com>
2014-08-19 08:10:56 -0700

    included the js files from last commit

---
+++ public/scripts/game/commands/WalkCommand.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="ICommand.ts" />
+/// <reference path="../unit.ts" />
+var WalkCommand = (function () {
+    function WalkCommand(location) {
+        this.name = "walk";
+        this.location = location;
+    }
+    WalkCommand.prototype.GetLocation = function () {
+        return this.location;
+    };
+
+    WalkCommand.prototype.ToString = function () {
+        return this.name;
+    };
+    return WalkCommand;
+})();
